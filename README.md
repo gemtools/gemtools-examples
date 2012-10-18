96217e57e1179a187806183fef28d4ad7b301369
Thasso Griebel <thasso.griebel@gmail.com>
2012-10-18 11:46:00 +0200

    Added boostrap script to download demo data

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 gemtools-examples
 =================
 
-GEM Tools example scripts
+Before you can start playing with the examples, run the bootstrap.sh script to 
+get the demo data, ie:
+
+$ > sh bootstrap.sh
+
+This will download the demo data into the newly created data folder
---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function download(){
+    file=$1
+    if [ ! -f "${file%.*}" ]; then
+        echo "Getting $file"
+        curl -L -O https://github.com/downloads/gemtools/gemtools-examples/${file}
+        gunzip ${file}
+    fi
+}
+
+if [ ! -d "data" ]; then
+    mkdir -p data
+fi
+
+cd data
+download "data_chr21.fastq.gz"
+download "chr21.fa.gz"
+
+
+
